55a0727f84afaf0bf9af0ab34e55e894b266679a
Jaime Duque <jaimeduque17@gmail.com>
2019-11-08 00:38:09 -0500

    The functionality is implemented

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import Phrase from './components/Phrase';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [phrase, getPhrase] = useState({});
+
+	const consultAPI = async () => {
+		const result = await axios('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+
+		// add API result to the state; (like a this.setState)
+
+		getPhrase(result.data[0]);
+	}
+
+	// query API Rest
+	useEffect(
+		() => {
+			consultAPI()
+		}, []
+
+	)
+
+	return (
+		<div className="container">
+			<Phrase
+				phrase={phrase}
+			/>
+			<button
+				onClick={consultAPI}
+			>New Phrase</button>
+		</div>
+	)
 }
 
 export default App;
